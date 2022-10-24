d499cc9f0e446cf3c7b809451a34ee2e79a775c9
chacho <charantej.chowdary.nalluri@nordnet.se>
2022-10-24 20:24:41 +0200

    jenkins integration

--- infrastructure/terraform/provider.tf
+++ infrastructure/terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = var.aws_region
-  access_key = "AKIATGJRTDLA3Q744XFM"
-  secret_key = "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"
+  access_key = "test"
+  secret_key = "test"
 
 }
